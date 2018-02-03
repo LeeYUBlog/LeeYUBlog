13b1c59cbc336e031e382257963024c4481d6b03
lhn <lhn@izengshi.com>
2018-02-03 23:01:17 +0800

    version0.1

---
+++ src/main/java/com/LeeYUBlog/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.LeeYUBlog.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/")
+	public String displayLanding(){
+		return "index";
+	}
+}
---
+++ src/main/java/com/LeeYUBlog/application/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.LeeYUBlog.application;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.LeeYUBlog.admin.controller")
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
+
+}
