3881b0b8b4b9bb81a67be91f3604864e22f6a7ff
lhn <lhn@izengshi.com>
2018-06-03 17:34:11 +0800

    01 add features leeYUBlog

--- leeyuBlog-web/src/main/java/com/LeeYUBlog/user/admin/controller/AdminController.java
+++ leeyuBlog-web/src/main/java/com/LeeYUBlog/user/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/admin")
 public class AdminController {
 
 	@RequestMapping("/hello")
@@ -11,8 +12,14 @@ public class AdminController {
 		return "hello";
 	}
 	
-	@RequestMapping(value="/admin")
+	@RequestMapping()
 	public String initAdmin() {
 		return "admin";
+	}
+	
+	//admintest页面
+	@RequestMapping("/dashboard.html")
+	public String testAdmin(){
+		return "dashboard";
 	}
 }
---
+++ leeyuBlog-web/src/main/resources/static/assets/js/gallery.js
@@ -0,0 +1,13 @@
+// Created by Avaz Bokiev @samarkandiy
+$(document).ready(function(){
+  $("[class^='thumbnail-']").click(function(){
+    //$("[class^='thumbnail-']").slideToggle("fast");
+    //$(this).next("[class^='large-']").slideToggle();
+  });
+  
+  $(".close").click(function(){
+    //$("[class^='large-']:visible").toggle();
+    //$("[class^='thumbnail-']").fadeToggle("fast");; 
+  }); 
+  
+});
